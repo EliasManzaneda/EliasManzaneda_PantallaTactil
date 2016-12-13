3fd3196072fab0ba5217d70cb81e0825be2778fc
EliasManzaneda <eliasmanzanedasantamaria@gmail.com>
2016-12-13 16:42:05 +0100

    Ejercicio5_8

---
+++ app/src/main/java/pantallatactil/example/com/pantalla_tactil/PantallaTactilActivity.java
@@ -0,0 +1,30 @@
+package pantallatactil.example.com.pantalla_tactil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class PantallaTactilActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        //5.8
+        TextView entrada = (TextView)findViewById(R.id.TextViewEntrada);
+        entrada.setOnTouchListener(this);
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        TextView salida = (TextView) findViewById(R.id.TextViewSalida);
+        salida.append("\n" + motionEvent.toString()+"\n" );
+
+
+        return true;
+    }
+}
